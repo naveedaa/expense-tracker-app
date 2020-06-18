9797cd3418bc2cc3bf2c3c82ca261689aaaec497
naveedaa <43065497+naveedaa@users.noreply.github.com>
2020-06-18 06:32:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import './App.css';
-
-//import Global Provider
 import { GlobalProvider } from './context/GlobalState' 
-
-// import components
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { AccountData } from './components/AccountData';
-// import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionData } from './components/TransactionData'
 import { AddTransaction } from './components/AddTransaction';
 
